70aec88e2eb403131ceae3f7e17717c962b15798
Bogdan Kootz <bodyakootz@gmail.com>
2015-05-26 21:30:18 +0300

    mockup around half

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,46 @@
-// d1 js
-$('.form_submit').on('click', function() {
-	$(this).closest('form').submit();
+// sliders
+// area
+$('.area_slider').noUiSlider({
+	start: [70, 800],
+	connect: true,
+	orientation: "horizontal",
+	range: {
+		'min': 70,
+		'max': 800
+	},
+	format: wNumb({
+		decimals: 0
+	})
 });
-// d2 js
+$('.area_slider').Link('lower').to($('#area_from'));
+$('.area_slider').Link('upper').to($('#area_to'));
+// yard_area
+$('.yard_area_slider').noUiSlider({
+	start: [1, 200],
+	connect: true,
+	orientation: "horizontal",
+	range: {
+		'min': 1,
+		'max': 200
+	},
+	format: wNumb({
+		decimals: 0
+	})
+});
+$('.yard_area_slider').Link('lower').to($('#yard_area_from'));
+$('.yard_area_slider').Link('upper').to($('#yard_area_to'));
+// price
+$('.price_slider').noUiSlider({
+	start: [400000, 800000000],
+	connect: true,
+	orientation: "horizontal",
+	range: {
+		'min': 400000,
+		'max': 800000000
+	},
+	format: wNumb({
+		decimals: 0
+	})
+});
+$('.price_slider').Link('lower').to($('#price_from'));
+$('.price_slider').Link('upper').to($('#price_to'));
--- resources/views/partials/filters.blade.php
+++ resources/views/partials/filters.blade.php
@@ -1,19 +1,47 @@
-<div class="filters">
-	<h3>поиск объекта</h3>
-	<div class="btn-group">
-		{{HTML::link('/', 'Купить', ['class' => 'btn btn-default'])}}
-		{{HTML::link('/', 'Арендовать', ['class' => 'btn btn-default'])}}
-	</div>
-	<div class="city_and_district">
-		{{ Form::label('city', 'Выбирите город', ['class'=>'']) }}
-		{{ Form::select('city', ['df'=>'dfs'], null, ['class'=>'form-control', 'required']) }}
-		{{ Form::label('district', 'Выбирите район', ['class'=>'']) }}
-		{{ Form::select('district', ['dfвыава'=>'dfавыаываs'], null, ['class'=>'form-control']) }}	
-	</div>
-	<div class="draggers">
-		<div class="area">
-			{{ Form::label('area', 'от', ['class'=>'']) }}
-			{{ Form::text('area')}}
+<div class="filters_full">
+	<div class="filters">
+		<h3>поиск объекта</h3>
+		<div class="selects">
+			<div class="btn-group">
+				{{HTML::link('/', 'Купить', ['class' => 'btn btn-default active'])}}
+				{{HTML::link('/', 'Арендовать', ['class' => 'btn btn-default'])}}
+			</div>
+			<div class="city_and_district">
+				<div class="city">
+					{{ Form::label('city', 'Выберите город', ['class'=>'filters_label']) }}
+					{{ Form::select('city', ['df'=>'dfs'], null, ['class'=>'form-control select_place select_city', 'required']) }}
+				</div>
+				<div class="district">
+					{{ Form::label('district', 'Выберите район', ['class'=>'filters_label']) }}
+					{{ Form::select('district', ['dfвыава'=>'dfавыаываs'], null, ['class'=>'form-control select_place select_district']) }}	
+				</div>
+			</div>
+		</div>
+		<div class="draggers">
+			<div class="area">
+				{{ Form::label('area_from', 'от', ['class'=>'filters_label']) }}
+				{{ Form::text('area_from', null, ['class'=>'form-control', 'id'=>'area_from'])}}
+				{{ Form::label('area_to', 'до', ['class'=>'filters_label']) }}
+				{{ Form::text('area_to', null, ['class'=>'form-control', 'id'=>'area_to'])}}
+				<span>м<sup>2</sup></span>
+				<div class="area_slider"></div>
+			</div>
+			<div class="yard_area">
+				{{ Form::label('yard_area_from', 'от', ['class'=>'filters_label']) }}
+				{{ Form::text('yard_area_from', null, ['class'=>'form-control', 'id'=>'yard_area_from'])}}
+				{{ Form::label('yard_area_to', 'до', ['class'=>'filters_label']) }}
+				{{ Form::text('yard_area_to', null, ['class'=>'form-control', 'id'=>'yard_area_to'])}}
+				<span>соток</span>
+				<div class="yard_area_slider"></div>
+			</div>
+			<div class="price">
+				{{ Form::label('price_from', 'от', ['class'=>'filters_label']) }}
+				{{ Form::text('price_from', null, ['class'=>'form-control', 'id'=>'price_from'])}}
+				{{ Form::label('price_to', 'до', ['class'=>'filters_label']) }}
+				{{ Form::text('price_to', null, ['class'=>'form-control', 'id'=>'price_to'])}}
+				<span>рублей</span>
+				<div class="price_slider"></div>
+			</div>
 		</div>
 	</div>
-</div>
+</div>	
