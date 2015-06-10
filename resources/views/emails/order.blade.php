669eac167395aecfb4fef50e9d74af3a31f20a50
Bogdan Kootz <bodyakootz@gmail.com>
2015-06-10 15:11:01 +0300

    minor changes

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -1,1 +1,29 @@
-{{ $name }}
+<div class="message_email">
+	<p>На вашем ресурсе <a href="http://www.vertex-shop.ru" target="_blank">Крым Вилла</a> была оставлена заявка!</p>
+</div>
+<table class="contacts_form_table">
+	<tr>
+		<td>Что интересует?</td>
+		<td>{{{$activity}}}</td>
+	</tr>
+	<tr>
+		<td>Имя:</td>
+		<td>{{{$name}}}</td>
+	</tr>
+	<tr>
+		<td>Контактный телефон:</td>
+		<td>{{{$phone}}}</td>
+	</tr>
+	<tr>
+		<td>E-mail:</td>
+		<td>{{{$email}}}</td>
+	</tr>
+	<tr>
+		<td>Тема письма:</td>
+		<td>{{{$theme}}}</td>
+	</tr>
+	<tr>
+		<td>Текст:</td>
+		<td>{{{$body}}}</td>
+	</tr>
+</table>
