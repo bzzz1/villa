0fa02c5ea3874419d65f550c128efba49f305e50
Bogdan Kootz <bodyakootz@gmail.com>
2015-06-10 19:47:19 +0300

    admin_articles_start

---
+++ resources/views/admin/estates.blade.php
@@ -0,0 +1,6 @@
+@extends('/layouts/admin_layout')
+@extends('/admin/header')
+@extends('/admin/sidebar')
+@extends('/partials/flash_messages')
+@extends('/admin/footer')
+@extends('/partials/estates')
