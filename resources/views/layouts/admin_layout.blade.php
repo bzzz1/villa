53de7a2676cfe460fc59e1f9e152e3c0d12b48fb
Bogdan Kootz <bodyakootz@gmail.com>
2015-06-26 15:28:17 +0300

    preloader

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -45,7 +45,15 @@
 
 			URL_IMG = "{{ url_path('estates') }}"; // resource
 
+			// PUBLIC = "{{public_path()}}"
+
+			// URL_ESTATE = "{{ ('img/photos/estates') }}"; // partial
+
 			URL_ESTATE = "{{ URL::to('/estates') }}"; // partial
+
+			URL_ESTATE_ADMIN = "{{ URL::to('/admin/change_estate') }}"; // admin
+
+			URL_ESTATE_ADMIN_DELETE = "{{ URL::to('/admin/delete_estate') }}"; // delete
 
 			URL_AJAX_ESTATES = "{{ l('ajax_estates') }}"; // full
 		/*----------------------------------------------*/
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -46,7 +46,16 @@
 				URL_IMG = "{{ url_path('estates') }}"; // resource
 
 				URL_ESTATE = "{{ URL::to('/estates') }}"; // partial
+
+				URL_ESTATE_ADMIN = "{{ URL::to('/admin/change_estate/') }}"; // admin
+
 				URL_AJAX_SELECT = "{{ URL::to('/ajax_select_estate') }}";
+				// PUBLIC = "{{public_path()}}"
+
+				URL_ESTATE_ADMIN = "{{ URL::to('/admin/change_estate') }}"; // admin
+
+				URL_ESTATE_ADMIN_DELETE = "{{ URL::to('/admin/delete_estate') }}"; // delete
+				
 
 				URL_AJAX_ESTATES = "{{ l('ajax_estates') }}"; // full
 
