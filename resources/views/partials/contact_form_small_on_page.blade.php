0eddf863014caa116c52c9b7db146e684430f850
Sergey <sbeznochenko@gmail.com>
2015-07-06 11:13:02 +0300

    add chnage for form_contact

--- resources/views/partials/contact_form_small_on_page.blade.php
+++ resources/views/partials/contact_form_small_on_page.blade.php
@@ -2,22 +2,22 @@
 	<h3>Написать нам</h3>
 	<hr class="form_hr">
 	<div class="form">
-		{{ Form::open(['url'=>'/feedback', 'method'=>'POST', 'class'=>'contact_form']) }}
+		{{ Form::open(['url'=>'/feedback', 'method'=>'POST', 'class'=>'contact_form', 'data-parsley-validate']) }}
 			<div class="inner_form_block">
 				<div class="name">
-					{{ Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Имя:'])}}
+					{{ Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Имя:', 'required'])}}
 					<i class="fa fa-chevron-right"></i>
 				</div>
 				<div class="phone">
-					{{ Form::text('phone', null, ['class'=>'form-control', 'placeholder'=>'Телефон:'])}}
+					{{ Form::text('phone', null, ['class'=>'form-control', 'placeholder'=>'Телефон:', 'required'])}}
 					<i class="fa fa-chevron-right"></i>
 				</div>
 				<div class="email">
-					{{ Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Email:'])}}
+					{{ Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Email:', 'required'])}}
 					<i class="fa fa-chevron-right"></i>
 				</div>
 				<div class="text_message">
-					{{ Form::textarea('text_message', null, ['class'=>'form-control', 'placeholder'=>'Сообщение'])}}
+					{{ Form::textarea('text_message', null, ['class'=>'form-control', 'placeholder'=>'Сообщение', 'required'])}}
 					<i class="fa fa-chevron-right"></i>
 				</div>
 				<div class="send btn js_send_contact_form">
